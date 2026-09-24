c3ad5ce4886871bf6decf3c9aa99f1fd17b3c7a7
Tim Torpy <133075783+TimothyTorpy@users.noreply.github.com>
2026-09-24 23:37:52 +0000

    Research for the bearings;

---
+++ src/triangulation.py
@@ -0,0 +1,9 @@
+
+# Zenith is height / elevation of the device from gps
+# GPS will give Long and Latt, Need to figure out Angle of the Dangle Next
+# Once we have a Heading (360 deg) then we calcualte the Angle (Use SIN, COS, DEG ??)
+# Putput Rotations. 
+
+# Research 
+# https://www.igismap.com/formula-to-find-bearing-or-heading-angle-between-two-points-latitude-longitude/
+# https://www.igismap.com/haversine-formula-calculate-geographic-distance-earth/
