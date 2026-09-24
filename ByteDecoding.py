008722904d747f431e2e454283ea08bee11e1307
Tim Torpy <133075783+TimothyTorpy@users.noreply.github.com>
2026-09-24 18:55:40 +0000

    Update Byte Decode, and grab;

--- ByteDecoding.py
+++ ByteDecoding.py
@@ -2,8 +2,11 @@
 LoraTestString = [0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x4C, 0x6F, 0x52, 0x61, 0x21]
 
 
-# def ByteDecode(startByte: int, endByte: int , byteArray: bytes):
-#     byteStart = startByte * 4
+def GetBytes(startByte:int , endByte: int, byteArray: bytes):
+    returnArry = []
+    for i in range(startByte, endByte):
+        returnArry.append(byteArray[i])
+    return returnArry
 
 
 def ByteTransform(byteArray: bytes):
@@ -15,4 +18,6 @@ def ByteTransform(byteArray: bytes):
 
 if __name__ == "__main__":
     print(ByteTransform(LoraTestString))
+    print(ByteTransform(GetBytes(0,3,LoraTestString)))
+    print(ByteTransform(GetBytes(5,8,LoraTestString)))
     
